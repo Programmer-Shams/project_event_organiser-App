9d8db21f8d4fcadbdc137952e939a30e0f96e046
ShamsTech005 <ssdeen313@gmail.com>
2023-12-21 21:28:47 +0100

    still working

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
+import { useForm, useFormContext } from "react-hook-form"
 import { Button } from "@/components/ui/button"
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
