dedaf9dada1984276d6c339e64519ea17797b66a
ShamsTech005 <ssdeen313@gmail.com>
2023-12-23 07:16:48 +0100

    few changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,9 +20,9 @@ module.exports = withUt({
     extend: {
       colors: {
         primary: {
-          500: '#624CF5',
+          500: '#0855d1',
           50: ' #F6F8FD',
-          DEFAULT: '#624CF5',
+          DEFAULT: '#0855d1',
           foreground: 'hsl(var(--primary-foreground))',
         },
         coral: {
